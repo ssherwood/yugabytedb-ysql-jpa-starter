4ecb6edc9cde4d23dbe5ac2b1ca58482b018f098
Shawn Sherwood <shawn.sherwood@gmail.com>
2024-06-20 11:37:57 -0400

    Update to latest Spring Boot and Flyway

--- src/main/java/io/undertree/starter/ysqljpa/config/RetryInterceptor.java
+++ src/main/java/io/undertree/starter/ysqljpa/config/RetryInterceptor.java
@@ -1,6 +1,11 @@
 package io.undertree.starter.ysqljpa.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryListener;
 import org.springframework.retry.RetryPolicy;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.retry.backoff.BackOffPolicy;
@@ -8,6 +13,9 @@ import org.springframework.retry.interceptor.RetryInterceptorBuilder;
 import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * RetryInterceptor is a Spring @Component that allows for method retries based
@@ -37,6 +45,19 @@ public class RetryInterceptor {
         retryTemplate.setRetryPolicy(retryPolicy);
         retryTemplate.setBackOffPolicy(backOffPolicy);
         return retryTemplate;
+    }
+
+    @Bean
+    public List<RetryListener> retryListeners() {
+        Logger log = LoggerFactory.getLogger(getClass());
+
+        return Collections.singletonList(new RetryListener() {
+            @Override
+            public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+                log.warn("I retried?");
+                RetryListener.super.onError(context, callback, throwable);
+            }
+        });
     }
 
     /**
